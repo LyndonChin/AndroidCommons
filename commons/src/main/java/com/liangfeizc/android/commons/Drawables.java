05b2b2e9a41ad00022e599be3b7ee48f15eb3c56
liangfei <njliangfei@gmail.com>
2016-01-26 15:25:05 +0800

    add Bitmaps

---
+++ commons/src/main/java/com/liangfeizc/android/commons/Drawables.java
@@ -0,0 +1,31 @@
+package com.liangfeizc.android.commons;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by liangfei on 1/26/16.
+ */
+public class Drawables {
+    public static Bitmap toBitmap(@NonNull  final Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int width = drawable.getIntrinsicWidth();
+        width = width > 0 ? width : 1;
+
+        int height = drawable.getIntrinsicHeight();
+        height = height > 0 ? height : 1;
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
