ce7cacc72d03be8b2879090f857922e557a0d8a9
liangfei <njliangfei@gmail.com>
2016-01-28 10:47:03 +0800

    add Urls

--- commons/src/main/java/com/liangfeizc/android/commons/Urls.java
+++ commons/src/main/java/com/liangfeizc/android/commons/Urls.java
@@ -1,7 +1,22 @@
 package com.liangfeizc.android.commons;
 
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+
 /**
  * Created by liangfei on 16/1/28.
  */
 public class Urls {
+    private static final String URL_REGEX = "^(http|https|ftp)\\://([a-zA-Z0-9\\.\\-]+(\\:[a-zA-"
+            + "Z0-9\\.&%\\$\\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{"
+            + "2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}"
+            + "[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|"
+            + "[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-"
+            + "4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0"
+            + "-9\\-]+\\.)*[a-zA-Z0-9\\-]+\\.[a-zA-Z]{2,4})(\\:[0-9]+)?(/"
+            + "[^/][a-zA-Z0-9\\.\\,\\?\\'\\\\/\\+&%\\$\\=~_\\-@]*)*$";
+
+    public static boolean isValid(@NonNull  final String url) {
+        return !TextUtils.isEmpty(url) && url.matches(URL_REGEX);
+    }
 }
